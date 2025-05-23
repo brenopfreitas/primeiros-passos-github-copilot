784a1f7ef98e3ff87e5bff44aa2488f925b4f3e5
Seu Nome <breno.pfreitas@bancobmg.com.br>
2025-05-23 16:04:09 -0300

    Add extracurricular activities to the in-memory database

--- src/app.py
+++ src/app.py
@@ -38,6 +38,45 @@ activities = {
       "schedule": "Segundas, quartas e sextas, 14h - 15h",
       "max_participants": 30,
       "participants": ["john@mergington.edu", "olivia@mergington.edu"]
+   },
+   # Esportivas
+   "Futebol": {
+      "description": "Participe do time de futebol da escola e jogue campeonatos",
+      "schedule": "Terças e quintas, 16h - 17h30",
+      "max_participants": 22,
+      "participants": ["lucas@mergington.edu", "marcos@mergington.edu"]
+   },
+   "Vôlei": {
+      "description": "Aulas e treinos de vôlei para todos os níveis",
+      "schedule": "Quartas e sextas, 15h - 16h30",
+      "max_participants": 18,
+      "participants": ["ana@mergington.edu", "carla@mergington.edu"]
+   },
+   # Artísticas
+   "Teatro": {
+      "description": "Oficina de teatro e expressão corporal",
+      "schedule": "Segundas, 16h - 17h30",
+      "max_participants": 15,
+      "participants": ["paulo@mergington.edu", "juliana@mergington.edu"]
+   },
+   "Clube de Artes": {
+      "description": "Desenho, pintura e outras atividades artísticas",
+      "schedule": "Sábados, 10h - 12h",
+      "max_participants": 20,
+      "participants": ["lara@mergington.edu", "bruno@mergington.edu"]
+   },
+   # Intelectuais
+   "Olimpíada de Matemática": {
+      "description": "Prepare-se para olimpíadas de matemática com aulas e desafios",
+      "schedule": "Quartas, 17h - 18h",
+      "max_participants": 25,
+      "participants": ["rafael@mergington.edu", "aline@mergington.edu"]
+   },
+   "Clube de Leitura": {
+      "description": "Leitura e discussão de livros clássicos e contemporâneos",
+      "schedule": "Sextas, 14h - 15h",
+      "max_participants": 16,
+      "participants": ["camila@mergington.edu", "pedro@mergington.edu"]
    }
 }
 
